00268611b84835ec3fc329493663a687398140ed
Damar <damarjokowi@gmail.com>
2017-09-25 01:23:53 +0700

    LEVEL 2  - Delete Records from Database with PHP

--- functions.php
+++ functions.php
@@ -32,4 +32,18 @@ function updateUserTable(){
 		die("Query Failed.");
 	}
 }
+
+function deleteUser(){
+	global $koneksi;
+	$id			  = $_POST['id'];
+	$username	  = $_POST['username'];
+	$password	  = $_POST['password'];
+
+	$query = "DELETE FROM user WHERE id = $id";
+	$result = mysqli_query($koneksi, $query);
+
+	if(!$result){
+		die("Query Failed.");
+	}
+}
 ?>
