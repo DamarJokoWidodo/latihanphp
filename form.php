10c7dc25a36c361a7bbed5a3c24bc2b8916a108e
Damar <damarjokowi@gmail.com>
2017-09-24 21:28:16 +0700

    LEVEL 1  - Checking for Form Submission

---
+++ form.php
@@ -0,0 +1,21 @@
+<?php
+if(isset($_POST['submit'])){
+	echo "Form di submit.";
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PHP forms</title>
+</head>
+<body>
+
+<form action="form.php" method="post">
+	<input type="text" placeholder="Enter Username" />
+	<input type="password" placeholder="Enter Password" />
+	<br />
+	<input type="submit" name="submit" />
+</form>
+
+</body>
+</html>
